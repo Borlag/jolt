d31e42a021fec7053aa85a356164cb282996b67b
Borlag <nabeg97@outlook.com>
2025-12-15 13:00:16 +0300

    Add graphical modeling scaffolding

--- jolt/ui/__init__.py
+++ jolt/ui/__init__.py
@@ -4,6 +4,7 @@ ENABLE_JOLT_BENCHMARKS = True
 
 from .components import render_sidebar, render_solution_tables, render_save_section
 from .state import initialize_session_state
+from .modeling import render_modeling_tab
 
 from .comparison import render_comparison_tab
 
@@ -13,6 +14,7 @@ __all__ = [
     "render_save_section",
     "initialize_session_state",
     "render_comparison_tab",
+    "render_modeling_tab",
     "ENABLE_JOLT_BENCHMARKS",
 ]
 
